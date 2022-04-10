459f1a86bd3a7cf91fdd6fd6bf8e5e69c2daa320
Afkre <ahmetefkere@gmail.com>
2022-04-10 13:46:10 +0200

    220410-1

--- src/components/mainPage/ProductCard.jsx
+++ src/components/mainPage/ProductCard.jsx
@@ -4,21 +4,21 @@ import { Link } from 'react-router-dom';
 const ProductCard = ({ productName, productId }) => {
     return (
         <>
-             <Card className='product-card' style={{width:'250px'}}>
-                <Card.Img  variant='top' src='vegetable.jpeg' />
+            <Card className='product-card' style={{ width: '250px' }}>
+                <Card.Img variant='top' src='vegetable.jpeg' />
                 <Card.Body>
                     <Card.Title>{productName}</Card.Title>
                     <Card.Text>
                         Fresh {productName} from our lovely farm
                     </Card.Text>
-                    <Button style={{display:'block', margin:'auto'}}>
-                        <Link to={`/detail/${productId}`} style={{color : 'white', textDecoration:'none'}}>
-                        Details
+                    <Button style={{ display: 'block', margin: 'auto' }}>
+                        <Link to={`/detail/${productId}`} style={{ color: 'white', textDecoration: 'none' }}>
+                            Details
                         </Link>
                     </Button>
                 </Card.Body>
             </Card>
         </>
     )
-}
+};
 export default ProductCard;
